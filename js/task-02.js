090c3e8a4f4e0f0f76ffee87e7461a3ab04df348
Gefeste <gefeste111@gmail.com>
2022-12-13 08:55:48 +0100

    update task 02 js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,34 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const potatoes = document.createElement('li');
+potatoes.classList.add('item');
+potatoes.textContent = ingredients[0];
+// console.log(potatoes)
+
+const mushrooms = document.createElement('li');
+mushrooms.classList.add('item');
+mushrooms.textContent = ingredients[1];
+
+const garlic = document.createElement('li');
+garlic.classList.add('item');
+garlic.textContent = ingredients[2];
+
+const tomatos = document.createElement('li');
+tomatos.classList.add('li');
+tomatos.textContent = ingredients[3];
+
+const herbs = document.createElement('li');
+herbs.classList.add('item')
+herbs.textContent = ingredients[4];
+
+const condiments = document.createElement('li');
+condiments.classList.add('item');
+condiments.textContent = ingredients[5];
+// console.log(condiments)
+
+const list = document.querySelector('#ingredients')
+// console.log(list)
+
+list.append(potatoes, mushrooms, garlic, tomatos,herbs, condiments)
