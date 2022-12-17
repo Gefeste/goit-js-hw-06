bade3702d9e25e9a3556a003be33f227ca3d2dd9
Gefeste <gefeste111@gmail.com>
2022-12-17 22:54:49 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const bodyEl = document.querySelector('body')
+const button = document.querySelector('.change-color')
+const textColor = document.querySelector('.color')
+
+button.addEventListener('click', changeBodyColor)
+
+function changeBodyColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+
+  textColor.textContent = bodyEl.style.backgroundColor
 }
