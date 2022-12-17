96a4271b5f4e50cfc77e5ff71943aed9f75df0da
Gefeste <gefeste111@gmail.com>
2022-12-17 18:49:13 +0100

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,21 @@
+const inputEl = document.querySelector('#validation-input');
+const inputCount = inputEl.dataset.length
+
+
+
+inputEl.addEventListener('blur', (event) => {
+    if (Number(inputCount) !== event.currentTarget.value) {
+        if (inputEl.classList.contains('invalid') === true) {
+              inputEl.classList.remove('invalid')
+        }
+        
+        inputEl.classList.add('valid')
+    } else {
+        if (inputEl.classList.contains('valid') === true) {
+              inputEl.classList.remove('valid')
+        }
+        
+        inputEl.classList.add('invalid')
+    }
+
+})
