095ee4d870b4a21d45bb6fe2ae9cc10464ff77c6
Gefeste <gefeste111@gmail.com>
2022-12-17 22:27:22 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,44 @@
 
+const form = document.querySelector('.login-form')
+console.log(form)
+
+const message = "всі поля повинні бути заповнені"
+
+const userData = {};
+
+form.addEventListener('submit', collectUserData)
+
+function collectUserData(event) {
+    event.preventDefault()
+    
+const {
+    elements: { email, password }
+} = event.currentTarget;
+    
+console.log(event.currentTarget)
+  if (email.value === "" || password.value === "") {
+    return alert(message);
+  }
+
+    userData.email = email.value;
+    userData.password = password.value;
+    console.log(userData)
+
+   form.reset()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
