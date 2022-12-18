c8fbaca5982011cce45d896f1d78d8e066c4d2a0
Gefeste <gefeste111@gmail.com>
2022-12-18 16:05:19 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -2,17 +2,7 @@ const item = document.querySelectorAll(".item")
 console.log("Number of categories:", item.length)
 
 
-
-const title = document.querySelectorAll('h2')
-const element = document.querySelectorAll('.item ul')
-
-
-title.forEach((el) => {
-    console.log("Category", el.textContent)
-
-    Array.from(element).map(elem => {
-       console.log("Elements", elem.children.length)
-    })
-
-
+item.forEach((ele) => {
+    console.log("Category", ele.firstElementChild.textContent)
+    console.log("Elements", ele.lastElementChild.children.length)
 })
