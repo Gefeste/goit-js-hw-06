a3001d4d17d9dec0d80edd1fb3d5c04325735150
Gefeste <gefeste111@gmail.com>
2022-12-18 13:12:59 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,33 +7,14 @@ const ingredients = [
   'Condiments',
 ];
 
-const potatoes = document.createElement('li');
-potatoes.classList.add('item');
-potatoes.textContent = ingredients[0];
-// console.log(potatoes)
 
-const mushrooms = document.createElement('li');
-mushrooms.classList.add('item');
-mushrooms.textContent = ingredients[1];
-
-const garlic = document.createElement('li');
-garlic.classList.add('item');
-garlic.textContent = ingredients[2];
-
-const tomatos = document.createElement('li');
-tomatos.classList.add('item');
-tomatos.textContent = ingredients[3];
-
-const herbs = document.createElement('li');
-herbs.classList.add('item')
-herbs.textContent = ingredients[4];
-
-const condiments = document.createElement('li');
-condiments.classList.add('item');
-condiments.textContent = ingredients[5];
-// console.log(condiments)
+const newArray = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item')
+  itemEl.textContent = ingredient
+  return itemEl
+})
 
 const list = document.querySelector('#ingredients')
-// console.log(list)
 
-list.append(potatoes, mushrooms, garlic, tomatos,herbs, condiments)
+list.append(...newArray)
